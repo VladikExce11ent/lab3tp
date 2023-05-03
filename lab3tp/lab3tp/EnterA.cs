af9761a01af8ca9ef1a7e6aebcf3ceb0766c09a8
VladikExce11ent <vkirutin@gmail.com>
2023-05-03 19:51:08 +0700

    sozdali takie vetki i sootvet fayli

---
+++ lab3tp/lab3tp/EnterA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Menu
+{
+    public class EnterA
+    {
+        public void Run()
+        {
+            Console.WriteLine("Введите значение A:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Вы ввели: " + a);
+        }
+    }
+}
---
+++ lab3tp/lab3tp/EnterB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Menu
+{
+    public class EnterB
+    {
+        public void Run()
+        {
+            Console.WriteLine("Введите значение В:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Вы ввели: " + b);
+        }
+    }
+}
